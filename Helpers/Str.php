613531f115d6e82e02d92a885c48f3ffcb28c14b
RyoDev <73240016+ryodevz@users.noreply.github.com>
2021-12-13 18:53:34 +0800

    Update Str.php

--- Helpers/Str.php
+++ Helpers/Str.php
@@ -4,12 +4,12 @@ namespace Helpers;
 
 class Str
 {
-    static function lowercase(string $string)
+    public static function lowercase(string $string)
     {
         return strtolower($string);
     }
 
-    static function uppercase(string $string)
+    public static function uppercase(string $string)
     {
         return strtoupper($string);
     }
